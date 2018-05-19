f7480824f9f58981c7cc8b1e94430ef407d00344
Anton Chepurov <anton.chepurov@gmail.com>
2018-05-19 17:36:29 +0300

    orm_enhanced: partition_by() method

--- orm_enhanced/models/models.py
+++ orm_enhanced/models/models.py
@@ -1,13 +1,22 @@
 from openerp.osv.orm import BaseModel
 
 
-def group_by(self, key):
+def group_by(self, key, as_dict=False, ensure_keys=()):
     res = {}
+    for k in ensure_keys: res[k] = []
     for x in self:
         by = x.mapped(key)
-        res.setdefault(by and by[0] or by, []).append(x.id)
+        res.setdefault(by[0] if by else by, []).append(x.id)
     for k in res:
         res[k] = self.browse(res[k])
-    return res.iteritems()
+    return res if as_dict else res.iteritems()
 
 BaseModel.group_by = group_by
+
+
+def partition_by(self, key, ensure_keys=()):
+    grouped = self.group_by(key, as_dict=True, ensure_keys=ensure_keys).items()
+    grouped.sort(key=lambda x: x[0])
+    return [x[1] for x in grouped]
+
+BaseModel.partition_by = partition_by
