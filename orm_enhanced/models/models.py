fee8f2dfde995db59230c48e9a96405dfae3428f
Anton Chepurov <anton.chepurov@gmail.com>
2022-01-06 17:28:50 +0200

    [FIX] orm_enhanced: TypeError: unhashable type: 'list'

--- orm_enhanced/models/models.py
+++ orm_enhanced/models/models.py
@@ -9,7 +9,7 @@ def group_by(self, key, as_dict=False, ensure_keys=()):
     for k in ensure_keys: res[k] = []
     for x in self:
         by = x.mapped(key)
-        res.setdefault(by[0] if by else by, []).append(x.id)
+        res.setdefault(by[0] if by else None if by == [] else by, []).append(x.id)
     for k in res:
         res[k] = self.browse(res[k])
     return res if as_dict else res.iteritems()
